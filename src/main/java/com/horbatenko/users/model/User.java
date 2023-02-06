f2814ea212838fb3774fbba3c66e37d16d7c33e5
volodymyr horbatenko <vhorbate@cisco.com>
2023-02-06 21:59:02 +0200

    fix constructor

--- src/main/java/com/horbatenko/users/model/User.java
+++ src/main/java/com/horbatenko/users/model/User.java
@@ -2,6 +2,7 @@ package com.horbatenko.users.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document("users")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     private ObjectId id;
